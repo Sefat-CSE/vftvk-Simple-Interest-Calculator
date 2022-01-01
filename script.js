50404dd3c518dc20e5526f5e9b5da8336ea0cd46
Sefat-CSE <95383877+Sefat-CSE@users.noreply.github.com>
2022-01-01 23:44:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+var result = document.getElementById("result");
 function compute()
 {
-    p = document.getElementById("principal").value;
+    if( document.getElementById("principal").value < 0){
+        alert("Enter a positive number");
+    }else{
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    result.textContent = `
+    If you deposit ${principal},
+at an interest rate of ${rate}.
+You will receive an amount of ${interest},
+in the year ${year}
+    
+    `;
+
+
+    }
+    
+    
     
 }
+     function updateRate(){
+        var rateval = document.getElementById("rate").value;
+        document.getElementById("rate_val").innerText = rateval;
+        
+    }
         
